5d801ca980cb75a83c0f29a13bfe5b5ed4448e66
thanaponmos <30825012+thanaponmos@users.noreply.github.com>
2017-10-10 15:06:37 +0700

    Initial commit

---
+++ src/Gumballl4achineTestDrive.java
@@ -0,0 +1,41 @@
+
+public class Gumballl4achineTestDrive {
+
+	
+	public static void main(String[] arqs){
+			GumballMachine gumballMachine = new GumballMachine(5);
+					
+			System.out.println (gumballMachine);
+			
+			gumballMachine.insertQuarter();
+			gumballMachine.turnCrank();
+			
+			System.out.println (gumballMachine);
+			
+			gumballMachine.insertQuarter();
+			gumballMachine.ejectQuarter();
+			gumballMachine.turnCrank ();
+			
+			System.out.println (gumballMachine);
+			
+			gumballMachine.insertQuarter();
+			gumballMachine.turnCrank ();
+			gumballMachine.insertQuarter();
+			gumballMachine.turnCrank ();
+			gumballMachine.ejectQuarter();
+			
+			System.out.println (gumballMachine);
+			
+			gumballMachine.insertQuarter();
+			gumballMachine.insertQuarter();
+			gumballMachine.turnCrank();
+			gumballMachine.insertQuarter();
+			gumballMachine.turnCrank ();
+			gumballMachine.insertQuarter();
+			gumballMachine.turnCrank ();
+			System.out.println(gumballMachine);
+	
+	
+	}
+	}
+	
